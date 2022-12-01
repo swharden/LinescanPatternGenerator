fed774cfd7dfbd7fdc44403cc56549ec832e1399
Scott W Harden <swharden@gmail.com>
2022-12-01 13:42:43 -0500

    sin pattern generator

--- src/Form1.Designer.cs
+++ src/Form1.Designer.cs
@@ -28,19 +28,175 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.nudOscillationCount = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nudPointCount = new System.Windows.Forms.NumericUpDown();
+            this.nudWidthPercent = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudAmplitudePercent = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nudOscillationCount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPointCount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidthPercent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmplitudePercent)).BeginInit();
             this.SuspendLayout();
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.formsPlot1.Location = new System.Drawing.Point(15, 101);
+            this.formsPlot1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(785, 590);
+            this.formsPlot1.TabIndex = 0;
+            // 
+            // nudOscillationCount
+            // 
+            this.nudOscillationCount.Location = new System.Drawing.Point(304, 62);
+            this.nudOscillationCount.Name = "nudOscillationCount";
+            this.nudOscillationCount.Size = new System.Drawing.Size(134, 31);
+            this.nudOscillationCount.TabIndex = 1;
+            this.nudOscillationCount.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(304, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Oscillations";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Points";
+            // 
+            // nudPointCount
+            // 
+            this.nudPointCount.Location = new System.Drawing.Point(24, 62);
+            this.nudPointCount.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.nudPointCount.Name = "nudPointCount";
+            this.nudPointCount.Size = new System.Drawing.Size(134, 31);
+            this.nudPointCount.TabIndex = 3;
+            this.nudPointCount.Value = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            // 
+            // nudWidthPercent
+            // 
+            this.nudWidthPercent.Location = new System.Drawing.Point(164, 62);
+            this.nudWidthPercent.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudWidthPercent.Name = "nudWidthPercent";
+            this.nudWidthPercent.Size = new System.Drawing.Size(134, 31);
+            this.nudWidthPercent.TabIndex = 5;
+            this.nudWidthPercent.Value = new decimal(new int[] {
+            80,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(164, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Width (%)";
+            // 
+            // nudAmplitudePercent
+            // 
+            this.nudAmplitudePercent.Location = new System.Drawing.Point(444, 62);
+            this.nudAmplitudePercent.Name = "nudAmplitudePercent";
+            this.nudAmplitudePercent.Size = new System.Drawing.Size(134, 31);
+            this.nudAmplitudePercent.TabIndex = 7;
+            this.nudAmplitudePercent.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(444, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Amplitude (%)";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(670, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 59);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Export";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(815, 705);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.nudAmplitudePercent);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nudWidthPercent);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nudPointCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudOscillationCount);
+            this.Controls.Add(this.formsPlot1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Linescan Pattern Generator";
+            ((System.ComponentModel.ISupportInitialize)(this.nudOscillationCount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPointCount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidthPercent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmplitudePercent)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
     #endregion
+
+    private ScottPlot.FormsPlot formsPlot1;
+    private NumericUpDown nudOscillationCount;
+    private Label label1;
+    private Label label2;
+    private NumericUpDown nudPointCount;
+    private NumericUpDown nudWidthPercent;
+    private Label label3;
+    private NumericUpDown nudAmplitudePercent;
+    private Label label4;
+    private Button button1;
 }
--- src/Form1.cs
+++ src/Form1.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace LinescanPatternGenerator;
 
 public partial class Form1 : Form
@@ -5,5 +7,76 @@ public partial class Form1 : Form
     public Form1()
     {
         InitializeComponent();
+        nudPointCount.ValueChanged += (s, e) => UpdatePlot();
+        nudWidthPercent.ValueChanged += (s, e) => UpdatePlot();
+        nudOscillationCount.ValueChanged += (s, e) => UpdatePlot();
+        nudAmplitudePercent.ValueChanged += (s, e) => UpdatePlot();
+        UpdatePlot();
+    }
+
+    private (double[] xs, double[] ys) GetPoints()
+    {
+        int points = (int)nudPointCount.Value;
+        double xWidth = (double)nudWidthPercent.Value / 100.0;
+        double xPad = (1 - xWidth) / 2;
+        double xStart = xPad;
+        double xEnd = 1 - xPad;
+        double xSpan = xEnd - xStart;
+
+        double oscillations = (double)nudOscillationCount.Value;
+        double amplitude = (double)nudAmplitudePercent.Value / 100;
+
+        double[] fracs = Enumerable.Range(0, points)
+            .Select(i => (double)i / (points - 1))
+            .ToArray();
+
+        double[] xs = fracs
+            .Select(frac => xStart + frac * xSpan)
+            .ToArray();
+
+        double[] ys = fracs
+            .Select(x => Math.Sin(x * Math.PI * 2 * oscillations))
+            .Select(x => amplitude * x / 2 + .5)
+            .ToArray();
+
+        return (xs, ys);
+    }
+
+    private void UpdatePlot()
+    {
+        (double[] xs, double[] ys) = GetPoints();
+
+        formsPlot1.Plot.Clear();
+        formsPlot1.Plot.AddScatter(xs, ys);
+        formsPlot1.Plot.SetAxisLimits(0, 1, 0, 1);
+        formsPlot1.Refresh();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        SaveFileDialog savefile = new()
+        {
+            FileName = $"linescan.xml",
+            Filter = "XML Files (*.xml)|*.xml|All files (*.*)|*.*"
+        };
+
+        if (savefile.ShowDialog() == DialogResult.OK)
+        {
+            SaveXML(savefile.FileName);
+        }
+    }
+
+    private void SaveXML(string path)
+    {
+        (double[] xs, double[] ys) = GetPoints();
+
+        StringBuilder sb = new();
+        sb.AppendLine("<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+        sb.AppendLine("<PVLinescanDefinition scanContinuous=\"False\" mode=\"freeHand\">");
+        for (int i = 0; i < xs.Length; i++)
+            sb.AppendLine($"  <PVFreehand x=\"{xs[i]}\" y=\"{ys[i]}\" />");
+        sb.AppendLine("</PVLinescanDefinition>");
+
+        File.WriteAllText(path, sb.ToString());
     }
 }
