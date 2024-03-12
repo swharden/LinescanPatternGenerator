c4cb4378fd96fe24cb36bf76a7adbb86834235d5
Scott W Harden <swharden@gmail.com>
2024-03-12 12:21:41 -0400

    improve deployment script

--- dev/deploy.bat
+++ dev/deploy.bat
@@ -1,4 +1,5 @@
 rmdir /S /Q "X:\Software\Linescan Pattern Generator"
 dotnet publish ../src/ --configuration Release
-robocopy "..\src\bin\Release\net6.0-windows\publish" "X:\Software\Linescan Pattern Generator" /E /NJH /NFL /NDL
+robocopy "..\src\bin\Release\net8.0-windows\publish" "X:\Software\Linescan Pattern Generator" /E /NJH /NFL /NDL
+explorer "X:\Software\Linescan Pattern Generator"
 pause
